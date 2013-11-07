7e5a0b2cb258addfca9a391e72131a013f6d3a9b
Yuriy A. Apollov <apollov@lgreenlabs.ru>
2013-11-07 23:24:22 +0400

    Fix exported variables and add make_current_dump function

--- .bash/10-functions.sh
+++ .bash/10-functions.sh
@@ -9,73 +9,67 @@ function gl {
 }
 
 function _get_projects_prefix {
-	echo ''
+	PROJECTS_PREFIX=''
+	echo $PROJECTS_PREFIX
 }
 
 function _get_projects_suffix {
-	echo `whoami`
+	PROJECTS_SUFFIX=`whoami`
+	echo $PROJECTS_SUFFIX
 }
 
 function _get_branch_name {
-	if [ -z $BRANCH_NAME ]
+	if [ -z $1 ]
 	then
-		if [ -z $1 ]
-		then
-			BRANCH_NAME=master
-		else
-			BRANCH_NAME=$1
-		fi
+		BRANCH_NAME=master
+	else
+		BRANCH_NAME=$1
 	fi
 	echo $BRANCH_NAME
 }
 
 function _get_site_name {
-	if [ -z $SITE_NAME ]
+	if [ -z $1 ]
 	then
-		if [ -z $1 ]
-		then
-			SITE_NAME=site
-		else
-			SITE_NAME=$1
-		fi
+		SITE_NAME=site
+	else
+		SITE_NAME=$1
 	fi
 	echo $SITE_NAME
 }
 
 function _get_project_name {
-	if [ -z $PROJECT_NAME ]
-	then
-		PROJECT_NAME=$1
-	fi
-	echo $PROJECT_NAME
+	echo $1
 }
 
-function go {
+function _get_project_root {
 	PROJECT_NAME=`_get_project_name $1`
 	if [ -z $PROJECT_NAME ]
 	then
 		echo Usage: "$0 "'<PROJECT_NAME> [SITE_NAME=site] [BRANCH_NAME=master]'
 		return 1
 	fi
-	export PROJECT_NAME
-	echo $PROJECT_NAME
-
 	SITE_NAME=`_get_site_name $2`
-	export SITE_NAME
-	echo $SITE_NAME
-
 	BRANCH_NAME=`_get_branch_name $3`
-	export BRANCH_NAME
-	echo $BRANCH_NAME
-
 	PROJECTS_PREFIX=`_get_projects_prefix`
-	echo $PROJECTS_PREFIX
-
 	PROJECTS_SUFFIX=`_get_projects_suffix`
-	echo $PROJECTS_SUFFIX
+	PROJECT_ROOT="$PROJECTS_PREFIX/$PROJECTS_ROOT/$PROJECT_NAME/$SITE_NAME/$PROJECTS_SUFFIX/$BRANCH_NAME"
+	echo $PROJECT_ROOT
+}
 
-	cd $PROJECTS_PREFIX/$PROJECTS_ROOT/$PROJECT_NAME/$SITE_NAME/$PROJECTS_SUFFIX/$BRANCH_NAME
-	pwd
+function go {
+	export PROJECT_NAME=`_get_project_name $1`
+	if [ -z $PROJECT_NAME ]
+	then
+		echo Usage: "$0 "'<PROJECT_NAME> [SITE_NAME=site] [BRANCH_NAME=master]'
+		return 1
+	fi
+	export SITE_NAME=`_get_site_name $2`
+	export BRANCH_NAME=`_get_branch_name $3`
+	export PROJECTS_PREFIX=`_get_projects_prefix`
+	export PROJECTS_SUFFIX=`_get_projects_suffix`
+	export PROJECT_ROOT="$PROJECTS_PREFIX/$PROJECTS_ROOT/$PROJECT_NAME/$SITE_NAME/$PROJECTS_SUFFIX/$BRANCH_NAME"
+	cd $PROJECT_ROOT
 	activate
 	cd $SOURCE_DIR_NAME
 }
@@ -85,4 +79,27 @@ function work {
 	read PROJECT_NAME
 	export PROJECT_NAME=$PROJECT_NAME
 	screen -S $PROJECT_NAME
+}
+
+function make_current_dump {
+	if [ -z $PROJECT_ROOT ]
+	then
+		echo First use \`go\` command to go into a project.
+		return 1
+	fi
+	dump_root="$PROJECT_ROOT/dumps"
+	date=`date '+%F_%T'`
+	dump_dir="$dump_root/$date"
+	db_filename="$PROJECT_NAME"_"$SITE_NAME"_`whoami`_"$BRANCH_NAME"
+	pgsql_filename=$dump_dir/pgsql.sql
+	pgsql_custom_filename=$dump_dir/pgsql.pg_dump
+	mysql_filename=$dump_dir/mysql.sql
+	echo $db_filename $dump_root $date $dump_dir $pgsql_filename $pgsql_custom_filename $mysql_filename
+	mkdir -p $dump_dir
+	pg_dump -O -Fp -c $db_filename > $pgsql_filename
+	pg_dump -Fc $db_filename > $pgsql_custom_filename
+	mysqldump -p $db_filename > $mysql_filename
+	ln -f -s $pgsql_filename $dump_root/latest.pgsql.sql
+	ln -f -s $pgsql_custom_filename $dump_root/latest.pgsql.pg_dump
+	ln -f -s $mysql_filename $dump_root/latest.mysql.sql
 }
